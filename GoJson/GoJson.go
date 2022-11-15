80d350e33e76d0f398e58522d29392b86dd35770
margaretalola <46435160+margaretalola@users.noreply.github.com>
2022-11-16 00:48:18 +0700

    Update GoJson.go

--- GoJson/GoJson.go
+++ GoJson/GoJson.go
@@ -17,7 +17,7 @@ type Mahasiswa struct {
 
 // make a function of struct
 func NewMahasiswa() []Mahasiswa {
-	mhs := []Mahasiswa{ //array; cause they're saving many data with the same type
+	mhs := []Mahasiswa{ 
 		Mahasiswa{
 			ID:   1,
 			NIM:  123454,
@@ -37,35 +37,6 @@ func NewMahasiswa() []Mahasiswa {
 	return mhs
 }
 
-/*
-	func PostMahasiswa(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		var mhs Mahasiswa
-		if r.Method == "POST" {
-			if r.Header.Get("Content-Type") == "application/json" {
-				decodeJson := json.NewDecoder(r.Body)
-				if err := decodeJson.Decode(&mhs); err != nil {
-					log.Fatal(err)
-				}
-			} else {
-				getID := r.PostFormValue("id")
-				id, _ := strconv.Atoi(getID)
-				getNim := r.PostFormValue("nim")
-				nim, _ := strconv.Atoi(getNim)
-				name := r.PostFormValue("name")
-				mhs = Mahasiswa{
-					ID:   id,
-					NIM:  nim,
-					Name: name,
-				}
-			}
-			dataMahasiswa, _ := json.Marshal(mhs)
-			w.Write(dataMahasiswa)
-			return
-		}
-		http.Error(w, "hayo mau ngapain", http.StatusNotFound)
-	}
-*/
 
 // make get function for mahasiswa
 func GetMahasiswa(w http.ResponseWriter, r *http.Request) {
@@ -84,7 +55,7 @@ func GetMahasiswa(w http.ResponseWriter, r *http.Request) {
 
 	http.Error(w, "hayo mau ngapain", http.StatusNotFound)
 }
-
+//main function for handlefunc
 func main() {
 	http.HandleFunc("/mahasiswa", GetMahasiswa)
 	fmt.Println("Server running...")
